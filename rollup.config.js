720c499f3bb787ac95a49facc7400bda09b0a682
Eser Ozvataf <eser@ozvataf.com>
2022-08-14 15:14:12 +0300

    added mock bundles.

--- rollup.config.js
+++ rollup.config.js
@@ -19,13 +19,6 @@ export default [
         sourcemap: true,
         exports: "named",
       },
-    ],
-  },
-  {
-    input: "src/index.ts",
-    plugins: [esbuild()],
-    external: externals,
-    output: [
       {
         file: pkg.module, // dist/mod.js
         format: "esm",
@@ -38,9 +31,49 @@ export default [
     input: "src/index.ts",
     plugins: [dts()],
     external: externals,
-    output: {
-      file: pkg.types, // dist/index.d.ts
-      format: "es",
-    },
+    output: [
+      {
+        file: "dist/index.d.ts",
+        format: "es",
+      },
+      {
+        file: "dist/mod.d.ts",
+        format: "es",
+      },
+    ],
+  },
+  {
+    input: "src/index.mock.ts",
+    plugins: [esbuild()],
+    external: externals,
+    output: [
+      {
+        file: "dist/index.mock.js",
+        format: "cjs",
+        sourcemap: true,
+        exports: "named",
+      },
+      {
+        file: "dist/mod.mock.js",
+        format: "esm",
+        sourcemap: true,
+        exports: "named",
+      },
+    ],
+  },
+  {
+    input: "src/index.mock.ts",
+    plugins: [dts()],
+    external: externals,
+    output: [
+      {
+        file: "dist/index.mock.d.ts",
+        format: "es",
+      },
+      {
+        file: "dist/mod.mock.d.ts",
+        format: "es",
+      },
+    ],
   },
 ];
